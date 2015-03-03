50d3dab2cc1b3172c8d90a08a61dd8369e7d00c0
laurabrooks <itslaurabrooks@gmail.com>
2015-03-02 22:53:54 -0500

    Wrote game.js

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,20 @@
-// Replace this with your code...
+// JavaScript for Ms. Pac Man game
+
+<!--
+
+function init(){
+
+	canvas = document.getElementById('game_canvas');
+	ctx = canvas.getContext('2d'); 
+
+	var img = new Image();
+	img.src = "pacman10-hp-sprite.png";
+
+
+	img.addEventListener("load", function() {
+		ctx.drawImage(img, 321, 0, 465, 138, 0, 0, 465, 138);
+		ctx.drawImage(img, 81, 22, 16, 16, 6, 6, 16, 16);
+	}, false);
+
+}
+-->
