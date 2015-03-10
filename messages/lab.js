049339f3b030d9e2f48047e449f5f15efbe069d6
laurabrooks <itslaurabrooks@gmail.com>
2015-03-09 22:12:19 -0400

    Wrote js file

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,24 @@
-// Your JavaScript goes here...
+//JavaScript for messages lab
+
+function parse(){
+	request = new XMLHttpRequest();
+	request.open("GET", "data.json", true);
+	request.onreadystatechange = parseData;
+	request.send();
+}
+
+function parseData(){
+	if (request.readyState == 4 && request.status == 200){
+		converted = JSON.parse(request.responseText);
+		divMessages = document.getElementById("messages");
+
+		for (i=0; i<converted.length; i++){
+			toAdd = "<p>" + (converted[i]["username"]) + ": " + (converted[i]["content"]) + "</p>" ;
+			divMessages.innerHTML += toAdd;
+		}
+	}
+
+	else if (request.readyState = 4 && request.status != 200){
+		alert("Page did not load");
+	}
+}
